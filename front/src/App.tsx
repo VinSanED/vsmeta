1003b6971bad6fa1c650159298283a7b00cba668
VinKSSan <vinsan.ed@proton.me>
2025-09-05 09:57:00 +0000

    first component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Nbtn from './components/notificationBTN/nbtn'
 
 function App() {
 
   return (
     <>
       <p>hello word</p>
+      <Nbtn/>
     </>
   )
 }
---
+++ front/src/components/notificationBTN/nbtn.tsx
@@ -0,0 +1,12 @@
+import nbtnIcon from "./../../assets/notification-icon.svg"
+import './styNbtn.css'
+function Nbtn() {
+
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={nbtnIcon} alt="Notificar" />
+    </div>
+  )
+}
+
+export default Nbtn;
